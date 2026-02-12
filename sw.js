c9243a88add15466dc06fcb182b40f9b8997044a
kevinaxeso2000-hash <kevinaxeso2000@gmail.com>
2026-02-11 23:59:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
